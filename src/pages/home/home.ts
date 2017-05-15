5040e82f8599a9ca65b50a16afce871b4eb5b452
likhona7 <likhona7>
2017-05-15 10:13:50 +0200

    latest

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LocationTracker } from '../../providers/location-tracker';
 import { Page2Page } from '../page2/page2';
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
-
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -13,16 +12,26 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 export class HomePage {
 
-customer: FirebaseListObservable<any>;
+location: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public locationTracker: LocationTracker, angFire: AngularFire) {
-     this.customer = angFire.database.list('/Customer');
+  constructor(public navCtrl: NavController, public locationTracker: LocationTracker, angFire: AngularFire, public zone: NgZone) {
+     this.location = angFire.database.list('/Location');
 
   }
 
-  start(){
+
+
+  start(longitude, latitude){
     this.locationTracker.startTracking();
+    this.location.push({
+    longitude: this.locationTracker.lng,
+    latitude: this.locationTracker.lat,
+    Created_date: Date()
+    }).then( error =>{
+    console.log(error);
+    });
   }
+
 
   stop(){
     this.locationTracker.stopTracking();
